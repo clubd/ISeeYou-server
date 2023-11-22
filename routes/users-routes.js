02c078468dd6a6b033dc54fea3ff2dad22b29e7b
clubd <danenrmar@gmail.com>
2023-11-22 09:03:03 -0500

    fix syntax

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const userController = require("../controllers/users-controller")
 
-router.route('/').get(userController.index);
+router.route("/").get(userController.index);
 router.route("/:id").get(userController.findOne);
 
 module.exports = router;
