542143ab7077fd5eee92ae2d3581e9b7e9f87e2b
clubd <danenrmar@gmail.com>
2023-11-22 08:46:06 -0500

    add tasks route

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const PORT = process.env.PORT || 8080;
 
 const userRoutes = require("./routes/users-routes");
 const tasksRoutes = require("./routes/tasks-routes");
+const assignsRoutes = require("./routes/assigns-routes");
 
 app.use("/users", userRoutes);
 app.use("/tasks", tasksRoutes);
+app.use("/assigns", assignsRoutes);
 
 
 app.listen(PORT, () => {
