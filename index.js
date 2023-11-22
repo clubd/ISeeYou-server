96d2cb73da4ff2bb4830b882c366257acb59518a
clubd <danenrmar@gmail.com>
2023-11-22 08:43:36 -0500

    add tasks routes

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 
 const userRoutes = require("./routes/users-routes");
+const tasksRoutes = require("./routes/tasks-routes");
 
 app.use("/users", userRoutes);
+app.use("/tasks", tasksRoutes);
 
 
 app.listen(PORT, () => {
