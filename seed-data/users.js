0dd8d26c99d21ad09adc60a243926816155ff300
clubd <danenrmar@gmail.com>
2023-11-21 23:11:31 -0500

    seed-data from users created

---
+++ seed-data/users.js
@@ -0,0 +1,34 @@
+const usersData = [
+    {
+        id: 1,
+        firstName: "Daniel",
+        lastName: "Martinez",
+        email: "danenrmar@gmail.com",
+        phone: "+17862059300",
+        password: "m1985m",
+    },
+    {
+        id: 2,
+        firstName: "Estefany",
+        lastName: "Claut",
+        email: "estefanyclautm@hotmail.com",
+        phone: "+17867922937",
+        password: "c1996c",
+    },
+    {
+        id: 3,
+        firstName: "Manuel",
+        lastName: "Martinez",
+        email: "delugo1956@gmail.com",
+        phone: "+15618665797",
+        password: "m1956m",
+    },
+    {
+        id: 4,
+        firstName: "Carlos",
+        lastName: "Martinez",
+        email: "sioncent@gmail.com",
+        phone: "+13056322980",
+        password: "c1989c",
+    },
+];
