7f8e3b73b843d5db438ac0e8078a73614f6533e5
clubd <danenrmar@gmail.com>
2023-11-22 09:01:04 -0500

    add controllers for users and to single user

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,13 +1,7 @@
-const knex = require("knex")(require("../knexfile"));
 const router = require("express").Router();
+const userController = require("../controllers/users-controller")
 
-router.get("/", async (_req, res) => {
-    try {
-        const data = await knex("users");
-        res.status(200).json(data);
-    } catch (err) {
-        res.status(400).send(`Error retrieving Users: ${err}`)
-    }
-});
+router.route('/').get(userController.index);
+router.route("/:id").get(userController.findOne);
 
 module.exports = router;
