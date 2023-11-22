edbfa7fb4f97443026fa7d04ffc564d74ed43858
clubd <danenrmar@gmail.com>
2023-11-21 22:57:03 -0500

    create tables assigns and tasks

--- migrations/20231122031910_create_users_tasks_assigns_tables.js
+++ migrations/20231122031910_create_users_tasks_assigns_tables.js
@@ -3,17 +3,49 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
-    createTable("users", (table) => {
-        table.increments("id").primary();
-        table.string("firstName").notNullable();
-        table.string("lastName").notNullable();
-        table.string("email").notNullable();
-        table.string('phone').notNullable();
-        table.string('password').notNullable();
-        table.timestamp("created_at").defaultTo(knex.fn.now());
-    })
+    return knex.schema
+        .createTable("users", (table) => {
+            table.increments("id").primary();
+            table.string("firstName").notNullable();
+            table.string("lastName").notNullable();
+            table.string("email").notNullable();
+            table.string('phone').notNullable();
+            table.string('password').notNullable();
+            table.timestamp("created_at").defaultTo(knex.fn.now());
+        })
+        .createTable("tasks", (table) => {
+            table.increments("id").primary();
+            table.string("title").notNullable();
+            able.text('description').notNullable();
+            table.string('status').notNullable();
+            table.string('priorityLevel').notNullable();
+            table.integer("users_id")
+                .unsigned()
+                .references("users.id")
+                .onUpdate("CASCADE")
+                .onDelete("CASCADE");
+            table.timestamp("created_at").defaultTo(knex.fn.now());
+            table
+                .timestamp("updated_at")
+                .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
+            table.timestamp("deadline").defaultTo(knex.fn.now());
+        })
+        .createTable("assigns", (table) => {
+            table.increments("id").primary();
+            tabletable.integer("users_id")
+                .unsigned()
+                .references("users.id")
+                .onUpdate("CASCADE")
+                .onDelete("CASCADE");
+            table.integer("tasks_id")
+                .unsigned()
+                .references("tasks.id")
+                .onUpdate("CASCADE")
+                .onDelete("CASCADE");
+            table.timestamp("created_at").defaultTo(knex.fn.now());
+        })
 };
 
 exports.down = function (knex) {
-
+    return knex.schema.dropTable("assigns").dropTable("tasks").dropTable("user");
 };
