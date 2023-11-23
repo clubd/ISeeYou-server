9d1a48c49571f2ab2bb797c07359d403e9ba9f51
clubd <danenrmar@gmail.com>
2023-11-23 12:29:22 -0500

    added router to get single task by id

--- controllers/tasks-controller.js
+++ controllers/tasks-controller.js
@@ -9,6 +9,27 @@ const index = async (_req, res) => {
     }
 };
 
+const findOne = async (req, res) => {
+    try {
+        const tasksFound = await knex("tasks")
+            .where({ id: req.params.id });
+
+        if (tasksFound.length === 0) {
+            return res.status(404).json({
+                message: `Task with ID ${req.params.id} not found`
+            });
+        }
+
+        const taskData = tasksFound[0];
+        res.json(taskData);
+    } catch (error) {
+        res.status(500).json({
+            message: `Unable to retrieve data for task with ID ${req.params.id}`,
+        });
+    }
+};
+
 module.exports = {
     index,
+    findOne,
 };
--- routes/tasks-routes.js
+++ routes/tasks-routes.js
@@ -2,5 +2,7 @@ const router = require("express").Router();
 const taskController = require("../controllers/tasks-controller");
 
 router.route("/").get(taskController.index);
+router.route("/:id").get(taskController.findOne);
+
 
 module.exports = router;
