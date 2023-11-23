db2a13e1d9cec6cb3995a370d722f991d2012a62
clubd <danenrmar@gmail.com>
2023-11-23 15:15:18 -0500

    add function for creation of a task involved a specific user

--- controllers/tasks-controller.js
+++ controllers/tasks-controller.js
@@ -29,7 +29,32 @@ const findOne = async (req, res) => {
     }
 };
 
+const create = async (req, res) => {
+    const userId = req.params.id;
+
+    if (!req.body.title || !req.body.description|| !req.body.status|| !req.body.priorityLevel) {
+        return res.status(400).json({
+            message: "Please provide all required information to create a task.",
+        });
+    }
+
+    try {
+        const taskData = {...req.body, users_id: userId};
+        const result = await knex("tasks").insert(taskData);
+        const newTaskId = result[0];
+        await knex("assigns").insert({ users_id: userId, tasks_id: newTaskId });
+        const createdTasks = await knex("tasks").where({ users_id: userId });
+
+        res.status(201).json(createdTasks);
+    } catch (error) {
+        res.status(500).json({
+            message: `Unable to create new task: ${error}`,
+        });
+    }
+};
+
 module.exports = {
     index,
     findOne,
+    create,
 };
--- routes/tasks-routes.js
+++ routes/tasks-routes.js
@@ -3,6 +3,6 @@ const taskController = require("../controllers/tasks-controller");
 
 router.route("/").get(taskController.index);
 router.route("/:id").get(taskController.findOne);
-
+router.route("/users/:id/tasks").post(taskController.create);
 
 module.exports = router;
