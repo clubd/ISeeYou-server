d44ab9dc7ecedc81ef6abf945cee19fb5f57ed7f
clubd <danenrmar@gmail.com>
2023-11-23 17:57:47 -0500

    add delete route and controller for users

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -11,8 +11,7 @@ const index = async (_req, res) => {
 
 const findOne = async (req, res) => {
     try {
-        const usersFound = await knex("users")
-            .where({ id: req.params.id });
+        const usersFound = await knex("users").where({ id: req.params.id });
 
         if (usersFound.length === 0) {
             return res.status(404).json({
@@ -31,9 +30,7 @@ const findOne = async (req, res) => {
 
 const tasks = async (req, res) => {
     try {
-        const tasks = await knex("users")
-            .join("tasks", "tasks.users_id", "users.id")
-            .where({ users_id: req.params.id });
+        const tasks = await knex("users").join("tasks", "tasks.users_id", "users.id").where({ users_id: req.params.id });
 
         res.json(tasks);
     } catch (error) {
@@ -65,9 +62,7 @@ const create = async (req, res) => {
 
 const update = async (req, res) => {
     try {
-        const rowsUpdated = await knex("users")
-            .where({ id: req.params.id })
-            .update(req.body);
+        const rowsUpdated = await knex("users").where({ id: req.params.id }).update(req.body);
 
         if (rowsUpdated === 0) {
             return res.status(404).json({
@@ -75,10 +70,7 @@ const update = async (req, res) => {
             });
         }
 
-        const updatedUser = await knex("users")
-            .where({
-                id: req.params.id,
-            });
+        const updatedUser = await knex("users").where({ id: req.params.id });
 
         res.json(updatedUser[0]);
     } catch (error) {
@@ -88,17 +80,33 @@ const update = async (req, res) => {
     }
 };
 
+const remove = async (req, res) => {
+    try {
+        const rowsDeleted = await knex("users").where({ id: req.params.id }).delete();
+
+        if (rowsDeleted === 0) {
+            return res.status(404).json({ message: `User with ID ${req.params.id} not found` });
+        }
+
+        res.sendStatus(204);
+    } catch (error) {
+        res.status(500).json({
+            message: `Unable to delete user: ${error}`
+        });
+    }
+};
+
 const createTask = async (req, res) => {
     const userId = req.params.id;
 
-    if (!req.body.title || !req.body.description|| !req.body.status|| !req.body.priorityLevel) {
+    if (!req.body.title || !req.body.description || !req.body.status || !req.body.priorityLevel || !req.body.deadline) {
         return res.status(400).json({
             message: "Please provide all required information to create a task.",
         });
     }
 
     try {
-        const taskData = {...req.body, users_id: userId};
+        const taskData = { ...req.body, users_id: userId };
         const result = await knex("tasks").insert(taskData);
         const newTaskId = result[0];
         await knex("assigns").insert({ users_id: userId, tasks_id: newTaskId });
@@ -118,5 +126,6 @@ module.exports = {
     tasks,
     create,
     update,
+    remove,
     createTask
 };
--- routes/users-routes.js
+++ routes/users-routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const userController = require("../controllers/users-controller")
 
 router.route("/").get(userController.index).post(userController.create);
-router.route("/:id").get(userController.findOne).patch(userController.update);
+router.route("/:id").get(userController.findOne).patch(userController.update).delete(userController.remove);
 router.route("/:id/tasks").get(userController.tasks).post(userController.createTask);
 
 module.exports = router;
