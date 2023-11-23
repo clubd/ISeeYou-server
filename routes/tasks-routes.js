5a2f2930e5bf63c68f31fce1428821cbfb625c46
clubd <danenrmar@gmail.com>
2023-11-23 18:01:43 -0500

    add route and controller for delete specific route

--- routes/tasks-routes.js
+++ routes/tasks-routes.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const taskController = require("../controllers/tasks-controller");
 
 router.route("/").get(taskController.index);
-router.route("/:id").get(taskController.findOne).patch(taskController.update);
+router.route("/:id").get(taskController.findOne).patch(taskController.update).delete(taskController.remove);
 
 module.exports = router;
