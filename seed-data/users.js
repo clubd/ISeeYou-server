aadf579ecd8fe6f6c80e0f746a81edf0fb8b9599
clubd <danenrmar@gmail.com>
2023-11-24 01:03:15 -0500

    fixed password for seed-data to show phashed password

--- seed-data/users.js
+++ seed-data/users.js
@@ -1,34 +1,36 @@
-module.exports = [
+const bcrypt = require('bcrypt');
+
+const saltRounds = 14;
+
+const users = [
     {
-        id: 1,
         firstName: "Daniel",
         lastName: "Martinez",
         email: "danenrmar@gmail.com",
         phone: "+17862059300",
-        password: "m1985m",
+        passwordHash: bcrypt.hashSync("m1985m", saltRounds),
     },
     {
-        id: 2,
         firstName: "Estefany",
         lastName: "Claut",
         email: "estefanyclautm@hotmail.com",
         phone: "+17867922937",
-        password: "c1996c",
+        passwordHash: bcrypt.hashSync("c1996c", saltRounds),
     },
     {
-        id: 3,
         firstName: "Manuel",
         lastName: "Martinez",
         email: "delugo1956@gmail.com",
         phone: "+15618665797",
-        password: "m1956m",
+        passwordHash: bcrypt.hashSync("m1956m", saltRounds),
     },
     {
-        id: 4,
         firstName: "Carlos",
         lastName: "Martinez",
         email: "sioncent@gmail.com",
         phone: "+13056322980",
-        password: "c1989c",
+        passwordHash: bcrypt.hashSync("c1989c", saltRounds),
     },
 ];
+
+module.exports = users;
