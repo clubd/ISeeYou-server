d23b8ac59c224b1cab573db7cbeb5da44419bfed
clubd <danenrmar@gmail.com>
2023-11-24 11:27:14 -0500

    add router for whisper api speech-to-tetxt functionality

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const cors = require("cors");
+const axios = require("axios");
+const multer = require("multer");
+const fs = require("fs");
 const PORT = process.env.PORT || 8080;
-const cors = require('cors');
+const API_KEY = process.env.WHISPER_API_KEY;
+
 
 const userRoutes = require("./routes/users-routes");
 const tasksRoutes = require("./routes/tasks-routes");
@@ -15,6 +20,33 @@ app.use("/users", userRoutes);
 app.use("/tasks", tasksRoutes);
 app.use("/assigns", assignsRoutes);
 
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+app.post("/speech-to-text", upload.single("audio"), async (req, res) => {
+    try {
+        const audioFile = req.file.buffer.toString("base64");
+        const response = await axios.post(
+            "https://api.openai.com/v1/whisper/async-transcription",
+            {
+                audio: audioFile,
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${WHISPER_API_KEY}`,
+                    "Content-Type": "application/json",
+                },
+            }
+        );
+
+        res.json(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: "Unable to perform action.",
+        });
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`running at http://localhost:${PORT}`);
