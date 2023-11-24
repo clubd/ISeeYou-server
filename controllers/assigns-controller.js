58b5a5254a9140fcbb7798f087f1d9872e71a946
clubd <danenrmar@gmail.com>
2023-11-23 19:22:34 -0500

    add controller and route to create assignment between user and task

--- controllers/assigns-controller.js
+++ controllers/assigns-controller.js
@@ -11,6 +11,31 @@ const index = async (_req, res) => {
     }
 };
 
+const add = async (req, res) => {
+    const userId = req.query.userId;
+    const taskId = req.query.taskId;
+
+    try {
+        const existingAssign = await knex("assigns").where({ users_id: userId, tasks_id: taskId });
+
+        if (existingAssign.length > 0) {
+            return res.status(400).json({
+                message: "Assignment already exists for this user and task.",
+            });
+        }
+
+        const result = await knex("assigns").insert({ users_id: userId, tasks_id: taskId });
+        const newAssignId = result[0];
+
+        res.status(201).json({ id: newAssignId, users_id: userId, tasks_id: taskId });
+    } catch (error) {
+        res.status(500).json({
+            message: `Unable to create assignment: ${error}`,
+        });
+    }
+};
+
 module.exports = {
-    index
+    index,
+    add
 }
--- routes/assigns-routes.js
+++ routes/assigns-routes.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const assignController = require("../controllers/assigns-controller")
 
 router.route("/").get(assignController.index);
+router.route("/add").post(assignController.add);
 
 module.exports = router;
